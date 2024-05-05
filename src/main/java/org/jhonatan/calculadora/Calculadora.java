8cfb49007df0c5bd0b552cd1242f70202d7e8ddc
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-05 11:04:13 -0500

    clase CALCULADORA con sobrecarga de metodos para sumar

--- src/main/java/org/jhonatan/calculadora/Calculadora.java
+++ src/main/java/org/jhonatan/calculadora/Calculadora.java
@@ -1,4 +1,3 @@
-
 package org.jhonatan.calculadora;
 
 /**
@@ -6,5 +5,40 @@ package org.jhonatan.calculadora;
  * @author Jhonatan
  */
 public class Calculadora {
-    
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public float sumar(float x, float y) {
+        return x + y;
+    }
+
+    public float sumar(int i, float j) {
+        return i + j;
+    }
+
+    public float sumar(float i, int j) {
+        return i + j;
+    }
+
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public int sumar(String a, String b) {
+        int resultado;
+        try {
+            //los convierte a enteros
+            resultado = Integer.parseInt(a) + Integer.parseInt(b);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+        return resultado;
+    }
+
+    public int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+
 }
